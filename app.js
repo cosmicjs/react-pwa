a20743a2c7ca903efcff9405e78b439059874606
a9kitkumar <a9kitsharma.com>
2019-01-24 23:32:08 +0530

    changed path

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('./'));
+app.use(express.static('/build/react-pwa'));
 
 app.listen(process.env.PORT || 5000, function () {
 });
