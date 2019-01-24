d9ac04029341e069aa3d2fdfad2e13f311994aac
a9kitkumar <a9kitsharma.com>
2019-01-24 23:20:31 +0530

    static path changed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('build/'));
+app.use(express.static('./'));
 
 app.listen(process.env.PORT || 5000, function () {
 });
