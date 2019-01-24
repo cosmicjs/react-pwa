55ad3779813b8b0dac48485799c43521b1104da1
a9kitkumar <a9kitsharma.com>
2019-01-24 23:43:05 +0530

    added bucket.json

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('/build/react-pwa'));
+app.use(express.static('./build'));
 
 app.listen(process.env.PORT || 5000, function () {
 });
